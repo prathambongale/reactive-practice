d533a9dd450041f2ead11dbeaa373ba74b311e10
Prathamesh Bongale <prathambongale@hotmail.com>
2020-12-03 14:44:44 +0200

    removed requirement comment from user service class

--- reactive-services/src/main/java/com/reactive/practice/services/UserService.java
+++ reactive-services/src/main/java/com/reactive/practice/services/UserService.java
@@ -28,31 +28,6 @@ public class UserService extends ResponseBuilder {
         this.sessionRepository = sessionRepository;
     }
 
-    /**
-     * Requirement:
-     * ------------
-     * Check if the session id is present in session collection.
-     *
-     * If Yes then update the user collection with new data that is passed in PersonalInfoRequest
-     * and return the session ID and success code back in response.
-     * Also validate the IDNumber for length if validation fails then return the CustomException.
-     * (In this sample code I am only validating the IDNumber but later I need to validate each and every field pass in the request)
-     *
-     * If No that is - session ID is empty then create the new User.
-     *
-     *
-     * Creat new user logic:
-     * --------------------
-     * -> While create a new user its mandatory to pass validator string. (create validator string using createValidatorString)
-     * -> If the validator string exists in validator collection then delete that string.
-     * -> Check if the IDNumber passed in PersonalInfo request exists in user DB.
-     * If yes then send a Custom Message saying "Record for the given IDNumber exists". If No then continue.
-     * -> Crete a new random session ID and user ID (_id in MongoDB) and save that in Session collection.
-     * -> Validate the IDNumber for length if validation fails then return the CustomException.
-     * (In this sample code I am only validating the IDNumber but later I need to validate each and every field pass in the request)
-     * -> Write the new user record in User collection and return newly created session ID and success code back in response.
-     *
-     * */
     public Mono<ResponseMessages> addUpdateUser(PersonalInfoRequest personalInfoRequest) {
         return sessionRepository.findBySid(personalInfoRequest.getId())
                 .log()
