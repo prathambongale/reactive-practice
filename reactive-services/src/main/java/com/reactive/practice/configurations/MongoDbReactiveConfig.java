63da8546aa05e81be128adcfda53435ef9054377
prathambongale <prathamesh.bongale@hotmail.com>
2020-12-07 20:43:02 +0200

    fixed MongoDB connection issue

--- reactive-services/src/main/java/com/reactive/practice/configurations/MongoDbReactiveConfig.java
+++ reactive-services/src/main/java/com/reactive/practice/configurations/MongoDbReactiveConfig.java
@@ -2,9 +2,12 @@ package com.reactive.practice.configurations;
 
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
-//@EnableReactiveMongoRepositories
+@Configuration
+@EnableReactiveMongoRepositories
 public class MongoDbReactiveConfig extends AbstractReactiveMongoConfiguration {
 
     @Override
